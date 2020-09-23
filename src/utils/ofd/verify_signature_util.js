84444b942404e91d98b3dbf0147f0f4eff07021e
Donal <donal.tong@gmail.com>
2020-09-23 22:11:43 +0800

    indexof大小写敏感

--- src/utils/ofd/verify_signature_util.js
+++ src/utils/ofd/verify_signature_util.js
@@ -11,29 +11,29 @@ import Base64 from "@lapo/asn1js/base64";
 export const digestByteArray = function(data,hashedBase64,checkMethod){
     const hashedHex = Uint8ArrayToHexString(Base64.decode(hashedBase64));
     checkMethod = checkMethod.toLowerCase()
-    if(checkMethod === "1.2.156.10197.1.401" || checkMethod === "sm3"){
-        console.log(checkMethod)
-        return hashedHex==sm3(Uint8ArrayToHexString(data));
-    }else if(checkMethod === "md5"){
-        return hashedHex==md5(data);
-    }else if(checkMethod === "sha1"){
-        return hashedHex==sha1(data);
+    if(checkMethod === "1.2.156.10197.1.401" || checkMethod.indexOf('sm3') !== -1){
+        return hashedHex === sm3(Uint8ArrayToHexString(data));
+    }else if(checkMethod.indexOf('md5') !== -1){
+        return hashedHex === md5(data);
+    }else if(checkMethod.indexOf('sha1') !== -1){
+        return hashedHex === sha1(data);
     }else{
         return "";
     }
 }
 
 export const SES_Signature_Verify = function(SES_Signature){
-    const signAlg = SES_Signature.toSign.version<4?SES_Signature.toSign.signatureAlgorithm:SES_Signature.signatureAlgID;
+    let signAlg = SES_Signature.toSign.version<4?SES_Signature.toSign.signatureAlgorithm:SES_Signature.signatureAlgID;
     const msg = SES_Signature.toSignDer;
+    signAlg = signAlg.toLowerCase();
     if(signAlg.indexOf("1.2.156.10197.1.501")>=0 || signAlg.indexOf("sm2")>=0){
         let sigValueHex = SES_Signature.signature.replace(/ /g,'').replace(/\n/g,'');
-        if(sigValueHex.indexOf('00')==0){
+        if(sigValueHex.indexOf('00')===0){
             sigValueHex = sigValueHex.substr(2,sigValueHex.length-2);
         }
         const cert = SES_Signature.toSign.version<4?SES_Signature.toSign.cert:SES_Signature.cert;
         let publicKey = cert.subjectPublicKeyInfo.subjectPublicKey.replace(/ /g,'').replace(/\n/g,'');
-        if(publicKey.indexOf('00')==0){
+        if(publicKey.indexOf('00')===0){
             publicKey = publicKey.substr(2,publicKey.length-2);
         }
         return sm2.doVerifySignature(msg, sigValueHex, publicKey, {
