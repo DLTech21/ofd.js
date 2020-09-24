0edffef7138106eaab95675b8d5138e7d206b646
roy19831015@gmail.com <roy19831015@gmail.com>
2020-09-24 09:53:05 +0800

    兼容签章版本号异常的情况

--- src/utils/ofd/verify_signature_util.js
+++ src/utils/ofd/verify_signature_util.js
@@ -10,46 +10,51 @@ import Base64 from "@lapo/asn1js/base64";
 
 export const digestByteArray = function(data,hashedBase64,checkMethod){
     const hashedHex = Uint8ArrayToHexString(Base64.decode(hashedBase64));
-    checkMethod = checkMethod.toLowerCase()
-    if(checkMethod === "1.2.156.10197.1.401" || checkMethod.indexOf('sm3') !== -1){
-        return hashedHex === sm3(Uint8ArrayToHexString(data));
-    }else if(checkMethod.indexOf('md5') !== -1){
-        return hashedHex === md5(data);
-    }else if(checkMethod.indexOf('sha1') !== -1){
-        return hashedHex === sha1(data);
+    checkMethod = checkMethod.toLowerCase();
+    if(checkMethod.indexOf("1.2.156.10197.1.401")>=0 || checkMethod.indexOf("sm3")>=0){
+        return hashedHex==sm3(Uint8ArrayToHexString(data));
+    }else if(checkMethod.indexOf("md5")>=0){
+        return hashedHex==md5(data);
+    }else if(checkMethod.indexOf("sha1")>=0){
+        return hashedHex==sha1(data);
     }else{
         return "";
     }
 }
 
 export const SES_Signature_Verify = function(SES_Signature){
-    let signAlg = SES_Signature.toSign.version<4?SES_Signature.toSign.signatureAlgorithm:SES_Signature.signatureAlgID;
-    const msg = SES_Signature.toSignDer;
-    signAlg = signAlg.toLowerCase();
-    if(signAlg.indexOf("1.2.156.10197.1.501")>=0 || signAlg.indexOf("sm2")>=0){
-        let sigValueHex = SES_Signature.signature.replace(/ /g,'').replace(/\n/g,'');
-        if(sigValueHex.indexOf('00')===0){
-            sigValueHex = sigValueHex.substr(2,sigValueHex.length-2);
+    try {
+        let signAlg = SES_Signature.realVersion<4?SES_Signature.toSign.signatureAlgorithm:SES_Signature.signatureAlgID;
+        signAlg = signAlg.toLowerCase();
+        const msg = SES_Signature.toSignDer;
+        if(signAlg.indexOf("1.2.156.10197.1.501")>=0 || signAlg.indexOf("sm2")>=0){
+            let sigValueHex = SES_Signature.signature.replace(/ /g,'').replace(/\n/g,'');
+            if(sigValueHex.indexOf('00')==0){
+                sigValueHex = sigValueHex.substr(2,sigValueHex.length-2);
+            }
+            const cert = SES_Signature.realVersion<4?SES_Signature.toSign.cert:SES_Signature.cert;
+            let publicKey = cert.subjectPublicKeyInfo.subjectPublicKey.replace(/ /g,'').replace(/\n/g,'');
+            if(publicKey.indexOf('00')==0){
+                publicKey = publicKey.substr(2,publicKey.length-2);
+            }
+            return sm2.doVerifySignature(msg, sigValueHex, publicKey, {
+                der : true,
+                hash: true,
+                userId:"1234567812345678"
+            });
+        }else{
+            let sig = new rsa.KJUR.crypto.Signature({"alg": "SHA1withRSA"});
+            const cert = SES_Signature.realVersion<4?SES_Signature.toSign.cert:SES_Signature.cert;
+            let sigValueHex = SES_Signature.signature.replace(/ /g,'').replace(/\n/g,'');
+            if(sigValueHex.indexOf('00')==0){
+                sigValueHex = sigValueHex.substr(2,sigValueHex.length-2);
+            }
+            sig.init(cert);
+            sig.updateHex(msg);
+            return sig.verify(sigValueHex);
         }
-        const cert = SES_Signature.toSign.version<4?SES_Signature.toSign.cert:SES_Signature.cert;
-        let publicKey = cert.subjectPublicKeyInfo.subjectPublicKey.replace(/ /g,'').replace(/\n/g,'');
-        if(publicKey.indexOf('00')===0){
-            publicKey = publicKey.substr(2,publicKey.length-2);
-        }
-        return sm2.doVerifySignature(msg, sigValueHex, publicKey, {
-            der : true,
-            hash: true,
-            userId:"1234567812345678"
-        });
-    }else{
-        let sig = new rsa.KJUR.crypto.Signature({"alg": "SHA1withRSA"});
-        const cert = SES_Signature.toSign.version<4?SES_Signature.toSign.cert:SES_Signature.cert;
-        let sigValueHex = SES_Signature.signature.replace(/ /g,'').replace(/\n/g,'');
-        if(sigValueHex.indexOf('00')==0){
-            sigValueHex = sigValueHex.substr(2,sigValueHex.length-2);
-        }
-        sig.init(cert);
-        sig.updateHex(msg);
-        return sig.verify(sigValueHex);
+    } catch (e) {
+        console.log(e)
+        return false;
     }
 }
