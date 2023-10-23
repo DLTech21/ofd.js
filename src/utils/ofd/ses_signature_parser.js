740d63e86d94436de2e766cc870d008814ff4a7e
1308774130 <39472661+1308774130@users.noreply.github.com>
2023-10-23 20:57:05 +0800

    fix: issues-电子发票-增值税发票无法显示电子签章 #98

--- src/utils/ofd/ses_signature_parser.js
+++ src/utils/ofd/ses_signature_parser.js
@@ -62,7 +62,7 @@ const decode = function (der, offset) {
         const SES_Signature = decodeSES_Signature(der,offset);
         const type = SES_Signature.toSign.eseal.esealInfo.picture.type;
         const ofdArray = SES_Signature.toSign.eseal.esealInfo.picture.data.byte;
-        return {ofdArray, 'type': type.toLowerCase(), SES_Signature,'verifyRet':SES_Signature_Verify(SES_Signature)};
+        return {ofdArray, 'type': typeof type === 'string'?type.toLowerCase():type.str.toLowerCase(), SES_Signature,'verifyRet':SES_Signature_Verify(SES_Signature)};
     } catch (e) {
         console.log(e)
         return {};
